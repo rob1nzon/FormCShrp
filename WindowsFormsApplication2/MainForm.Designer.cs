4101547ebe20d7ecf0913eb01bca476e82892a61
Denis <denis@rob1nzon.ru>
2015-05-28 13:09:34 +0300

    add_all

--- WindowsFormsApplication2/MainForm.Designer.cs
+++ WindowsFormsApplication2/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication2
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -39,7 +39,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Parameters = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Value1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.FieldName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Sol = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Filter = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -76,7 +90,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(199, 316);
+            this.listBox1.Size = new System.Drawing.Size(199, 368);
             this.listBox1.TabIndex = 2;
             this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             // 
@@ -95,7 +109,6 @@
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -105,7 +118,7 @@
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.Location = new System.Drawing.Point(213, 65);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(346, 316);
+            this.dataGridView1.Size = new System.Drawing.Size(346, 252);
             this.dataGridView1.TabIndex = 5;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -145,7 +158,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(370, 12);
+            this.textBox2.Location = new System.Drawing.Point(370, 13);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(117, 20);
             this.textBox2.TabIndex = 7;
@@ -153,19 +166,125 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(576, 12);
+            this.groupBox1.Controls.Add(this.dataGridView3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(565, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(412, 369);
+            this.groupBox1.Size = new System.Drawing.Size(363, 421);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings tree";
             // 
-            // Form1
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Parameters,
+            this.Value,
+            this.Value1});
+            this.dataGridView3.Location = new System.Drawing.Point(9, 51);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(343, 364);
+            this.dataGridView3.TabIndex = 20;
+            // 
+            // Parameters
+            // 
+            this.Parameters.HeaderText = "Parameters";
+            this.Parameters.Name = "Parameters";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // Value1
+            // 
+            this.Value1.HeaderText = "Value1";
+            this.Value1.Name = "Value1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Type";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "regression",
+            "classifiction"});
+            this.comboBox4.Location = new System.Drawing.Point(57, 24);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(85, 21);
+            this.comboBox4.TabIndex = 11;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            this.comboBox4.SelectedValueChanged += new System.EventHandler(this.comboBox4_SelectedValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(148, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 21);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FieldName,
+            this.Sol,
+            this.Filter});
+            this.dataGridView2.Location = new System.Drawing.Point(213, 323);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(346, 110);
+            this.dataGridView2.TabIndex = 9;
+            this.dataGridView2.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView2_RowsAdded);
+            // 
+            // FieldName
+            // 
+            this.FieldName.HeaderText = "FieldName";
+            this.FieldName.Name = "FieldName";
+            // 
+            // Sol
+            // 
+            this.Sol.HeaderText = "Sol";
+            this.Sol.Items.AddRange(new object[] {
+            "=",
+            ">",
+            "<",
+            ">=",
+            "<="});
+            this.Sol.Name = "Sol";
+            // 
+            // Filter
+            // 
+            this.Filter.HeaderText = "Filter";
+            this.Filter.Name = "Filter";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1000, 455);
+            this.ClientSize = new System.Drawing.Size(933, 455);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -174,9 +293,13 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,6 +318,17 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewComboBoxColumn FieldName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Sol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Filter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Parameters;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value1;
     }
 }
 
---
+++ WindowsFormsApplication2/MainForm.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using CsvHelper;
+using Microsoft.VisualBasic;
+using System.Xml;
+
+namespace WindowsFormsApplication2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public void open_csv()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "CSV Files|*.csv";
+            openFileDialog1.Title = "Select a CSV";
+
+            // Show the Dialog.
+            // If the user clicked OK in the dialog and
+            // a .CUR file was selected, open it.
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.
+                //this.Cursor = new Cursor(openFileDialog1);
+                var cr = new StreamReader(openFileDialog1.FileName);
+                var csv = new CsvReader(cr);
+                string hF = "";
+                while (csv.Read())
+                {
+                    hF = csv.FieldHeaders[0];
+                    break;
+                }
+                dataGridView1.RowCount = 1;
+                string[] slis = hF.Split(new Char[]{';',','});
+                foreach (string news in slis){
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                    row.Cells[0].Value = news;
+                    dataGridView1.Rows.Add(row);
+                }
+                textBox1.Text = openFileDialog1.FileName;
+                
+            }
+        }
+
+        public void load_table()
+        {
+            foreach (string name in PG_class.load_table_name(textBox1.Text))
+                        listBox1.Items.Add(name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string typeSource = comboBox1.SelectedItem.ToString().Trim();
+            switch (typeSource)
+            {
+                case "SQL":
+                    load_table();
+                    break;
+                case "CSV":
+                    open_csv();
+                    break;
+                    
+                default:
+                    break;
+                    
+            }
+         
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            PG_class.select_table = listBox1.SelectedItem.ToString();
+            dataGridView1.RowCount = 1;
+            dataGridView1.ColumnCount = 3;
+            foreach (string name in PG_class.load_colum_name(textBox1.Text, listBox1.SelectedItem.ToString())){
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = name;
+                row.Cells[1].Value = false;
+                row.Cells[2].Value = false;
+                dataGridView1.Rows.Add(row);
+                (dataGridView2.Rows[0].Cells[0] as DataGridViewComboBoxCell).Items.Add(name);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (comboBox1.SelectedItem == "SQL") {
+                textBox1.Text = "Server=localhost;Port=5432;User=postgres;Password=root;Database=postgis_21_sample;";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+
+            List<string> all_field = new List<string>();
+            List<string> input_field = new List<string>();
+            List<string> output_field = new List<string>();
+            
+            string sql_query = "SELECT ";
+            for (int i = 1; i < dataGridView1.RowCount; i++) { 
+                sql_query += dataGridView1.Rows[i].Cells[0].Value.ToString() + ", ";
+                if ((dataGridView1.Rows[i].Cells[1] as DataGridViewCheckBoxCell).Value.ToString() == "True")                    
+                    input_field.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                if ((dataGridView1.Rows[i].Cells[2] as DataGridViewCheckBoxCell).Value.ToString() == "True")
+                    output_field.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                all_field.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+            sql_query= sql_query.Remove(sql_query.Length - 2, 1);
+            sql_query += " FROM " + PG_class.select_table;
+            if (dataGridView2.RowCount > 1)
+            {
+                sql_query += " WHERE ";
+                for (int i = 0; i < dataGridView2.RowCount-1; i++)
+                {
+                    dataGridView2.EndEdit();
+                    sql_query += (dataGridView2.Rows[i].Cells[0] as DataGridViewComboBoxCell).Value.ToString() + " " +
+                        (dataGridView2.Rows[i].Cells[1] as DataGridViewComboBoxCell).Value.ToString() + " " +
+                        (dataGridView2.Rows[i].Cells[2] as DataGridViewTextBoxCell).Value.ToString() + " AND ";
+                }
+                sql_query = sql_query.Remove(sql_query.Length - 2, 1);
+            }
+            //MessageBox.Show(sql_query);
+            
+            SaveXML(ref dataGridView3, sql_query, all_field, input_field, output_field);
+                             
+
+        }
+
+        private static void SaveXML(ref DataGridView df, string sql_query,List<string> all_field,List<string> input_field, List<string> output_field)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "XML Files|*.xml";
+            saveFileDialog1.Title = "Save a XML";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                XmlTextWriter textWritter = new XmlTextWriter(saveFileDialog1.FileName, Encoding.UTF8);
+                textWritter.WriteStartDocument();
+                textWritter.WriteStartElement("source");
+                textWritter.WriteStartElement("sql");
+                textWritter.WriteString(sql_query);
+                textWritter.WriteEndElement();
+
+                foreach (string ipn in input_field)
+                {
+                    textWritter.WriteStartElement("input");
+                    textWritter.WriteString(ipn);
+                    textWritter.WriteEndElement();
+                }
+
+                foreach (string ipn in output_field)
+                {
+                    textWritter.WriteStartElement("output");
+                    textWritter.WriteString(ipn);
+                    textWritter.WriteEndElement();
+                }
+                MessageBox.Show(df.Rows[1].Cells[0].Value.ToString());
+                for (int i = 1; i < df.RowCount-1; i++)
+                {
+                    textWritter.WriteStartElement(df.Rows[i].Cells[0].Value.ToString());
+                    textWritter.WriteStartAttribute("Value");
+                    textWritter.WriteString(df.Rows[i].Cells[1].Value.ToString());
+                    textWritter.WriteStartAttribute("Value1");
+                    textWritter.WriteString(df.Rows[i].Cells[2].Value.ToString());
+                    textWritter.WriteEndElement();
+                }
+                
+                textWritter.WriteEndElement();
+                
+               
+                textWritter.Close();
+                
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView2_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            foreach (string nams in PG_class.tbl_name){
+                (dataGridView2.Rows[dataGridView2.RowCount-1].Cells[0] as DataGridViewComboBoxCell).Items.Add(nams);
+            }
+        }
+
+        private void comboBox4_SelectedValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView3.Rows.Clear();
+            if (comboBox4.SelectedItem == "regression")
+            {
+                string[] param = {"", "criterion", "splitter", "max_features", "max_depth","min_samples_split",
+                             "min_samples_leaf", "min_weight_fraction_leaf","max_leaf_nodes"};
+                string[][] param_option = new string[param.Length][];
+                param_option[0] = new string[0] { };
+                param_option[1] = new string[2] { "gini", "entropy" };
+                param_option[2] = new string[2] { "best", "random" };
+                param_option[3] = new string[5] { "int", "auto", "sqrt", "log2", "None" };
+                param_option[4] = new string[1] { "int" };
+                param_option[5] = new string[1] { "int" };
+                param_option[6] = new string[1] { "int" };
+                param_option[7] = new string[1] { "int" };
+                param_option[8] = new string[1] { "int" };
+                int i = 0;
+                foreach (string one_p in param)
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView3.Rows[0].Clone();
+                    row.Cells[0].Value = one_p;
+                    foreach (string one in param_option[i])
+                        (row.Cells[1] as DataGridViewComboBoxCell).Items.Add(one);
+                    //(row.Cells[2] as DataGridViewTextBoxCell)
+                    row.Cells[1].Value = "";
+                    row.Cells[2].Value = "";
+                    dataGridView3.Rows.Add(row);
+                    i++;
+                }
+            }
+        }
+
+ 
+    }
+
+}
+
+static class PG_class
+{
+    static public string select_table;
+    static public List<string> tbl_name;
+    static public List<string> sql_query(String connectionString, String query)
+    {
+        NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+        npgSqlConnection.Open();
+        NpgsqlCommand npgSqlCommand = new NpgsqlCommand(query, npgSqlConnection);
+        NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader();
+        
+        List<string> name_table = new List<string>();
+        if (npgSqlDataReader.HasRows)
+        {
+            while (npgSqlDataReader.Read())
+            {
+                name_table.Add(npgSqlDataReader[0].ToString());
+            }
+        }
+        npgSqlConnection.Close();
+        return name_table;        
+    }
+
+
+    static public List<string> load_table_name(String connectionString)
+    {
+        return sql_query(connectionString, "select tablename as table from pg_tables where schemaname = 'agz_'");
+    }
+    static public List<string> load_colum_name(String connectionString, String table_name)
+    {
+        tbl_name = sql_query(connectionString, string.Format("SELECT column_name FROM information_schema.columns  WHERE table_schema='agz_' AND table_name='{0}'", table_name));
+        return tbl_name;
+    }
+}
+
+
